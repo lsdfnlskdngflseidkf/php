a1bc9dd905c9106cd593e038a2c4af76734e631e
lsdfnlskdngflseidkf <139208267+lsdfnlskdngflseidkf@users.noreply.github.com>
2023-12-21 16:19:13 +0545

    Add files via upload

---
+++ eighth.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "simple arithmetic operators<br/>";
+    $a=5;
+    $b=6;
+    $c=$a+$b;
+    echo "sum of $a and $b is $c <br>";
+    $c=$a-$b;
+    echo "difference of $a and $b is $c <br>";
+    $c=$a*$b;
+    echo "product of $a and $b is $c <br>";
+    $c=$a/$b;
+    echo "dividend of $a and $b is $c <br>";
+    $c=$a%$b;
+    echo "modulo of $a and $b is $c <br>";
+    $c=$a**$b;
+    echo "$a^$b is ".$c."<br>";
+    ?>
+</body>
+</html>
---
+++ ninth.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "simple arithmetic operators <br>";
+    $a=30;
+    $b=6;
+    $a+=$b;
+    echo "a is 30 <br>";
+    echo "sum of a and $b is $a <br>";
+    $a=30;
+    $a-=$b;
+    echo "difference of a and $b is $a <br>";
+    $a=30;
+    $a*=$b;
+    echo "product of a and $b is $a <br>";
+    $a=30;
+    $a/=$b;
+    echo "dividend of a and $b is $a <br>";
+    $a=30;
+    $a%=$b;
+    echo "modulo of a and $b is $a <br>";
+    $a=30;
+    $a**=$b;
+    echo "a^$b is $a <br>";
+    ?>
+</body>
+</html>
