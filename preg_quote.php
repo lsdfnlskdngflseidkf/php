23afdec99c50ead93ce69e01db4bc9b5cd2d0be3
BMCBim <something123sdhgk@proton.me>
2024-01-25 11:50:49 +0545

    Uploading preg_quote

--- preg_quote.php
+++ preg_quote.php
@@ -1,10 +1,10 @@
 <?php
-$searchstring=preg_quote("://","/");
+$searchstring=preg_quote('://','/');
 $input="https://www.bmcbkt.edu.np";
 $pattern="/$searchstring/";
 if(preg_match($pattern,$input)){
-    echo "URL";
+    echo "This is a URL";
 }
 else
-    echo "not URL";
+    echo "This is not a URL";
 ?>
