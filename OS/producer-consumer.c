f8478b4c63b76127d0f49cee924bbcd9371d9bf2
NullProj <par@ujwalp.com.np>
2024-02-05 12:00:19 +0545

    added os's things

---
+++ OS/producer-consumer.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+int mutex=1;
+int full=0;
+int empty=10,x=0;
+void producer(){
+    //Decrease Mutex value by 1 
+    --mutex;
+    //increase number of full slots by 1
+    ++full;
+    //Decrease the number of empty slots by 1
+    --empty;
+    //item produced
+    x++;
+    printf("\n Producer Produces item %d ",x);
+    //increase Mutex value by 1
+}
+//function to consume an item and remove it from the buffer
+void consumer(){
+    //Decrease Mutex value by 1
+    --mutex;
+    //decrease the number of full slots by 1
+    --full;
+    //increase the number of empty slots by 1 
+    ++empty;
+    printf("Consumer consumes item %d ",x);
+    x--;
+
+}
+//driver code
+int main(){
+    int n,i;
+    printf("\n press 1 for producer \n press 2 for consumer  \n press 3 for Exit");
+    for (i=1;i>0;i++){
+        printf("\n Enter Your Choice:");
+        scanf("%d",&n);
+
+        switch(n){
+                case 1:
+                    if((mutex==1)&&( empty!=0)){
+                        producer();
+                    }
+            else {
+                printf("Buffer is Full");
+            }
+        break;
+        case 2:
+                if((mutex==1) && (full!=0)){
+                    consumer();
+                }
+            else {
+                printf("Buffer is Empty");
+            }           
+            break;
+        case 3:
+        exit(0);
+        break;
+        }
+    }
+}
