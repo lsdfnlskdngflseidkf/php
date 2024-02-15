49c11ff9cf32a62a2d85fa22e77abd0cdead3f5f
BMCBim <something123sdhgk@proton.me>
2024-02-15 16:32:57 +0545

    testing mysql

---
+++ database/connecting.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  
+  
+
+   $servername="Localhost"; 
+   $username="root";
+   $password="";
+   $database="Ujwal";
+
+   $connection=new Mysqli 
+  ?>
+</body>
+</html>
