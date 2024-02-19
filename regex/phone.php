06ccea857944f6188bedea9c0fc6741c6b01d30d
NullProj <par@ujwalp.com.np>
2024-02-19 19:20:28 +0545

    can't make this work

--- regex/phone.php
+++ regex/phone.php
@@ -1,12 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$number="9749844275";
-$patntc="/^97(0-9){8}$/";
-$patnt="/^984(0-9){7}$/";
-$patnc="/^98(0-1){1}(0-9){7}$/";
-if(preg_match($patntc,$number) || preg_match($patnt,$number)){
+$number = "9749844275";
+$patntc = "/^97(0-9){8}$/";
+$patnt = "/^984(0-9){7}$/";
+$patnc = "/^98(0-1){1}(0-9){7}$/";
+
+if (preg_match($patntc, $number) || preg_match($patnt, $number)) {
     echo "Your phone number is NTC";
-}
-elseif(preg_match($patntc,$number)){
+} elseif (preg_match($patnc, $number)) {
     echo "Your Number is Ncell";
 }
 ?>
+</body>
+</html>
