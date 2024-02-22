245d915fcc8746f68f1266fd5139d28849bb7a52
BMCBim <something123sdhgk@proton.me>
2024-02-22 15:53:12 +0545

    softdelete table is already altered

--- database/softdelete.php
+++ database/softdelete.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST['id'];
     $alterquery="ALTER TABLE travelbooking
 ADD is_deleted boolean default false; ";
-$connection->query($alterquery);
+// $connection->query($alterquery);
 
     $updateQuery = "UPDATE travelbooking SET  is_deleted= 1 WHERE id = '$id'";
     if($connection->query($updateQuery)){
