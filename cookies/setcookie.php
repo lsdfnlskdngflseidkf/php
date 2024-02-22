4f047bc41bd5f2a607dc7f2b35f03446d03f030a
BMCBim <something123sdhgk@proton.me>
2024-02-22 16:14:52 +0545

    updated cookie script

--- cookies/setcookie.php
+++ cookies/setcookie.php
@@ -5,8 +5,10 @@
 </head>
 <body>
    <?php
-  setcookie('User',"John doe",time()+3600,"/") ;
-  setcookie('User2',"jane doe",time()+3600,"/","ujwalp.com.np") ;
+//   setcookie('User',"John doe",time()+3600,"/") ;
+//   setcookie('User2',"jane doe",time()+3600,"/","ujwalp.com.np") ;
+  $user=isset($_COOKIE['user'])?$_COOKIE['user']:"Guest";
+echo "welcome ".$user; 
    ?> 
 </body>
 </html>
