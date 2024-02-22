f4560aef2b1fc28aa8271bdca11bc0d9641d74b1
BMCBim <something123sdhgk@proton.me>
2024-02-22 16:23:38 +0545

    cookie stuff fixed

--- cookies/setcookie.php
+++ cookies/setcookie.php
@@ -5,8 +5,9 @@
 </head>
 <body>
    <?php
-//   setcookie('User',"John doe",time()+3600,"/") ;
-//   setcookie('User2',"jane doe",time()+3600,"/","ujwalp.com.np") ;
+  setcookie("user","John doe",time()+3600,"/") ;
+  setcookie("user","janice doe",time()+3600,"/") ;//modify the cookie name to janice doe
+  setcookie('user',"John doe",time()-2600,"/") ;//deleting the cookie
   $user=isset($_COOKIE['user'])?$_COOKIE['user']:"Guest";
 echo "welcome ".$user; 
    ?> 
