9cd0368132969122b297cd1f9430796cc82370a2
Nullproj <par@ujwalp.com.np>
2024-05-12 21:36:35 +0545

    something

--- Labworks/unit-6/2/session.php
+++ Labworks/unit-6/2/session.php
@@ -5,4 +5,12 @@
    $_SESSION['email']="ujwalparajuli9@gmail.com";
    $_SESSION['start_time']=time();//record the session starting time
    echo "You have successfully saved your session";
+   if(isset($_SESSION['username']) && isset($_SESSION['password'])){
+        echo "Welcome " . $_SESSION['username']."<br>";
+        echo "Your email is " . $_SESSION['email'];
+        echo "<br>";
+    } else {
+        echo "You haven't logged in yet. Please login first to continue";
+    }
+session_destroy();
    ?>
